3e4b5f7b26531c9b2959cc69b270d5637c4840ad
phil9l <phil9lne@gmail.com>
2017-06-09 20:18:33 +0500

    Add tokenizing function

--- CodeHighlighter/CodeHighlighter/Application/Highlighter.cs
+++ CodeHighlighter/CodeHighlighter/Application/Highlighter.cs
@@ -1,8 +1,8 @@
 ﻿using System.Collections.Generic;
 using CodeHighlighter.Domain;
+using System;
 
-namespace CodeHighlighter.Application
-{
+namespace CodeHighlighter.Application {
     internal class Highlighter
     {
         private Dictionary<TokenTypes, string> tokenTransformDict = new Dictionary<TokenTypes, string>
@@ -54,5 +54,41 @@ namespace CodeHighlighter.Application
                 TokenTypes.Decorator, "decorator"
             }
         };
+
+        public IEnumerable<ResultToken> TokenizeSourceCode(string source, BaseTokenizer tokenizer) {
+            string[] lines = source.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+            var written = new Position(1, 0);
+            foreach (var tok in tokenizer.Tokenize(lines)) {
+                string tType = tok.Type.ToString();
+                string tStr = tok.Content;
+
+                var startPos = new Position(tok.Start.LineIndex, tok.Start.CharIndex);
+                var endPos = new Position(tok.End.LineIndex, tok.End.CharIndex);
+                var currentToken = new ResultToken(tStr, tType);
+
+                string resultType;
+                if (!tokenTransformDict.TryGetValue(tok.Type, out resultType)) {
+                    resultType = "";
+                }
+
+                if (resultType != "") {
+                    if (!written.Equals(startPos)) {
+                        text, written = _combine_range(lines, written, start_pos);
+                        if (text != "") {
+                            yield return new ResultToken(text, "");
+                        }
+                    }
+                    var text = currentToken.Content;
+                    written = endPos;
+                    if (text != "") {
+                        yield return new ResultToken(text, resultType);
+                    }
+                }
+            }
+            line_upto_token, written = _combine_range(lines, written, end_pos);
+            if (line_upto_token != "") {
+                yield return new ResultToken(line_upto_token, '');
+            }
+        }
     }
 }
--- CodeHighlighter/CodeHighlighter/Domain/Position.cs
+++ CodeHighlighter/CodeHighlighter/Domain/Position.cs
@@ -3,6 +3,22 @@
     public struct Position
     {
         private int lineIndex, charIndex;
+        public int LineIndex {
+            get {
+                return lineIndex;
+            }
+            set {
+                lineIndex = value;
+            }
+        }
+        public int CharIndex {
+            get {
+                return charIndex;
+            }
+            set {
+                charIndex = value;
+            }
+        }
         private object p;
 
         public Position(int lineIndex, object p) : this()
